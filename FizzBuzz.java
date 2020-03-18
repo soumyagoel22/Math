ba4a55f0d0f967942d8cd994cbfea86d4ffeaa76
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-03-18 19:24:21 +0530

    FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,34 +2,6 @@ import java.util.*;
 
 public class Solution {
     public ArrayList<String> fizzBuzz(int A) {
-        // int i;
-        // String j;
-        // ArrayList<String> B=new ArrayList<String>();
-        // String a1="FizzBuzz";
-        // String a2="Fizz";
-        // String a3="Buzz";
-        // for(i=1;i<=A;i++)
-        // {
-        //     // j=Integer.toString(i);
-        //     if(i%3==0 && i%5==0)
-        //     {
-        //         B.add(a1);
-        //     }
-        //      else if(i%3==0)
-        //     {
-        //         B.add(a2);
-        //     }
-        //     else if(i%5==0)
-        //     {
-        //         B.add(a3);
-        //     }
-        //      else
-        //     {
-        //          j=Integer.toString(i);
-        //         B.add(j);
-        //     }
-        // }
-        // return B;
         int i;
         ArrayList<String> B = new ArrayList<String>();
         String a = "Fizz";
