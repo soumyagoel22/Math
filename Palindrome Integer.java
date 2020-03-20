744688f8f1fd4a7070f36bde9cd42e0df7346e68
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-03-20 11:25:28 +0530

    Palindrome Integer

--- Palindrome Integer.java
+++ Palindrome Integer.java
@@ -1,47 +1,7 @@
+import java.util.*;
+
 public class Solution {
     public int isPalindrome(int A) {
-    //     int i,n,n1,c,rem,rev,rem1,k,flag=0,r,r1;
-    //     c=0;
-    //     k=A;
-    //     rev=0;
-    //     n=A;
-    //     n1=A;
-    //     if(A>=0 && A<=9)
-    //     {
-    //         return 1;
-    //     }
-    //     while(n>0)
-    //     {
-    //         rem=n%10;
-    //         c++;
-    //         n=n/10;
-    //     }
-    //   for(i=0;i<c;i++)
-    //   {
-    //       rem1=n1%10;
-    //       rev=rev*10+rem1;
-    //       n1=n1/10;
-    //   }
-    //   for(i=0;i<c;i++)
-    //   {
-    //       r=k%10;
-    //       r1=rev%10;
-    //       if(r==r1)
-    //       {
-    //           flag=1;
-    //           k=k/10;
-    //           rev=rev/10;
-    //       }
-    //       else
-    //       {
-    //           flag=0;
-    //           break;
-    //       }
-    //   }
-    //   if(flag==0)
-    //   return 0;
-    //   else
-    //   return 1;
     String a = Integer.toString(A);
     int i,j,n;
     n=a.length();
