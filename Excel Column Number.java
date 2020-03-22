298a76caa5a630b31ee4081437ecaee2ddca7e4b
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-03-23 00:02:31 +0530

    Excel Column Number

--- Excel Column Number.java
+++ Excel Column Number.java
@@ -4,7 +4,6 @@ public class Solution {
     public int titleToNumber(String A) {
          int result = 0;
         for (int i = 0; i < A.length(); i++) {
-            // result *= 26;
             result += A.charAt(i) - 'A' + 1;
         }
         return result;
