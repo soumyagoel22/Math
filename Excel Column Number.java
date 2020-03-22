e9201cd8d5461e8e5409a1322538b504121559f5
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-03-22 23:58:43 +0530

    Create Excel Column Number.java

---
+++ Excel Column Number.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int titleToNumber(String A) {
+    //     int i,n,j1,k,k1,sum;
+    //     char j;
+    //     k1=65;
+    //     sum=0;
+    //     n=A.length();
+    //     for(i=0;i<n;i++)
+    //     {
+    //         j=A.charAt(i);
+    //         k=Integer.parseInt(String.valueOf(j));
+    //         for(j1=1;j1<27;j1++)
+    //         {
+    //             if(k==k1)
+    //             {
+    //                 sum=sum+j1;
+    //                 break;
+    //             }
+    //             k1++;
+    //         }
+    //     }
+    //     return sum;
+         int result = 0;
+        for (int i = 0; i < A.length(); i++) {
+            // result *= 26;
+            result += A.charAt(i) - 'A' + 1;
+        }
+        return result;
+    }
+}
